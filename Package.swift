db60260d9b7c4c8138a676b4a8a3f71680619acc
Charles Wright <cvwright@futo.org>
2022-12-09 09:28:40 -0600

    New approach based on matrix-rust-components-swift

--- Package.swift
+++ Package.swift
@@ -3,6 +3,10 @@
 
 import PackageDescription
 
+let version = "0.1.5"
+let url = "https://github.com/matrix-org/matrix-rust-sdk/releases/download/matrix-sdk-crypto-ffi-\(version)/MatrixSDKCryptoFFI.zip"
+let checksum = "1c662db291ea422ca3c8079ddb790df293f3caa839ede6b03b895f999347f358"
+
 let package = Package(
     name: "MatrixSDKCrypto",
     platforms: [
@@ -12,15 +16,30 @@ let package = Package(
         .macOS(.v12)
     ],
     products: [
-        .library(name: "MatrixSDKCrypto", targets: ["MatrixSDKCryptoFFI"]),
+        .library(
+            name: "MatrixSDKCrypto",
+            targets: ["MatrixSDKCrypto"]
+        ),
     ],
     dependencies: [
     ],
     targets: [
         .binaryTarget(
             name: "MatrixSDKCryptoFFI",
-            url: "https://github.com/matrix-org/matrix-rust-sdk/releases/download/matrix-sdk-crypto-ffi-0.1.5/MatrixSDKCryptoFFI.zip",
-            checksum: "1c662db291ea422ca3c8079ddb790df293f3caa839ede6b03b895f999347f358"
+            url: url,
+            checksum: checksum
+        ),
+        /*
+            Apparently we can't build our Swift package library directly from the FFI target.
+            We need a sort of intermediate build target as a go-between.
+            This technique comes from the Matrix Rust components package: 
+            https://github.com/matrix-org/matrix-rust-components-swift/blob/f4ff25f54c7e854fa1498f6dfd7bedca8c0ed260/Package.swift#L32
+        */
+        .target(
+            name: "MatrixSDKCrypto",
+            dependencies: [
+                .target(name: "MatrixSDKCryptoFFI")
+            ]
         )
     ]
 )
